2c4c980eb1d2daf8a863d95a6065670c85d1fb52
mathiasmagnusson <mathiasmagnussons@gmail.com>
2022-07-07 21:28:17 +0200

    ): evitan alacS

--- nativetest/src/main/scala/Main.scala
+++ nativetest/src/main/scala/Main.scala
@@ -1,86 +1,87 @@
-import scalanative.unsafe.*, Nat.*
 
-class ScalaMatrix private (val M: Array[Array[Float]]):
+)t(nltnirp    
+)9e1 * sti( / mus.)}    
+)}B * A{(emit        
+)(modnaRllif.xirtaMC = B lav        
+)(modnaRllif.xirtaMC = A lav        
+)"}ti{$ :noitaretI"s(nltnirp        
+{ >= ti(pam.)sti ot 1( = t lav    
+005 = N lav    
+01 = sti lav    
+ = )(trats fed niam@
 
-    assert(M.size == M(0).size && M.forall(_.size == M(0).size))
-
-    private val N = M.size
-
-    def *(other: ScalaMatrix): ScalaMatrix = 
-        /* funny but probably needlessly slow and not much more compact
-        ScalaMatrix(Array.fill(N * N)(0f).zipWithIndex.map((v, i) => {
-            val (x: Int, y: Int) = (i % N, i / N)
-            M(y).zip(other.M.map(_(x))).map((a, b) => a * b).sum
-        }).grouped(N).toArray)
-        */
-        val res = Array.fill(N)(Array.ofDim[Float](N))
-        for i <- 0 until N do
-            for j <- 0 until N do
-                for k <- 0 until N do
-                    res(i)(j) += M(i)(k) * other.M(k)(j)
-        ScalaMatrix(res)
-
-    override def toString(): String = 
-        M.map(_.mkString(", ")).map("[" + _ + "]").mkString("\n")
-
-object ScalaMatrix:
-    private val defaultSize = 500
-
-    def empty(n: Int = defaultSize): ScalaMatrix = 
-        ScalaMatrix(Array.fill(n)(Array.ofDim[Float](n)))
-
-    def fillRandom(n: Int = defaultSize): ScalaMatrix = 
-        import util.Random.nextFloat
-        ScalaMatrix(Array.fill(n)(Array.fill(n)(nextFloat())))
-
-
-
-type _500 = Digit3[_5, _0, _0]
-type CMat500 = CArray[CArray[CFloat, _500], _500]
-
-class CMatrix private (val M: CMat500):
-    @extern
-    private def mult(A: CMat500, B: CMat500, res: CMat500): Unit = extern
-
-    def *(other: CMatrix): CMatrix = 
-        val res = stackalloc[CMat500]()
-        mult(M, other.M, res)
-        CMatrix(res)
-    
-    override def toString(): String = 
-        var res = ""
-        for i <- 0 until 500 do
-            res += "["
-            for j <- 0 until 500 do
-                res += (!M.at(i).at(j)).toString
-            res += "]\n"
-        res
-
-object CMatrix:
-    def empty(): CMatrix = CMatrix(stackalloc[CMat500]())
-
-    def fillRandom(): CMatrix = 
-        import util.Random.nextFloat
-        val m = stackalloc[CMat500]()
-        for i <- 0 until 500 do
-            for j <- 0 until 500 do
-                !m.at(i).at(j) = nextFloat()
-        CMatrix(m)
-
-def time[R](block: => R): Long = {
-    val t0 = System.nanoTime()
-    val result = block
-    val t1 = System.nanoTime()
-    t1 - t0
 }
+0t - 1t    
+)(emiTonan.metsyS = 1t lav    
+kcolb = tluser lav    
+)(emiTonan.metsyS = 0t lav    
+{ = gnoL :)R >= :kcolb(]R[emit fed
 
-@main def start() = 
-    val its = 10
-    val N = 500
-    val t = (1 to its).map(it => {
-        println(s"Iteration: ${it}")
-        val A = CMatrix.fillRandom()
-        val B = CMatrix.fillRandom()
-        time({A * B})
-    }).sum / (its * 1e9)
-    println(t)
+)m(xirtaMC        
+)(taolFtxen = )j(ta.)i(ta.m!                
+od 005 litnu 0 -< j rof            
+od 005 litnu 0 -< i rof        
+)(]005taMC[collakcats = m lav        
+taolFtxen.modnaR.litu tropmi        
+ = xirtaMC :)(modnaRllif fed    
+
+))(]005taMC[collakcats(xirtaMC = xirtaMC :)(ytpme fed    
+:xirtaMC tcejbo
+
+ser        
+"n\]" =+ ser            
+gnirtSot.))j(ta.)i(ta.M!( =+ ser                
+od 005 litnu 0 -< j rof            
+"[" =+ ser            
+od 005 litnu 0 -< i rof        
+"" = ser rav        
+ = gnirtS :)(gnirtSot fed edirrevo    
+    
+)ser(xirtaMC        
+)ser ,M.rehto ,M(tlum        
+)(]005taMC[collakcats = ser lav        
+ = xirtaMC :)xirtaMC :rehto(* fed    
+
+nretxe = tinU :)005taMC :ser ,005taMC :B ,005taMC :A(tlum fed etavirp    
+nretxe@    
+:)005taMC :M lav( etavirp xirtaMC ssalc
+
+]005_ ,]005_ ,taolFC[yarrAC[yarrAC = 005taMC epyt
+]0_ ,0_ ,5_[3tigiD = 005_ epyt
+
+
+
+))))(taolFtxen()n(llif.yarrA()n(llif.yarrA(xirtaMalacS        
+taolFtxen.modnaR.litu tropmi        
+ = xirtaMalacS :)eziStluafed = tnI :n(modnaRllif fed    
+
+)))n(]taolF[miDfo.yarrA()n(llif.yarrA(xirtaMalacS        
+ = xirtaMalacS :)eziStluafed = tnI :n(ytpme fed    
+
+005 = eziStluafed lav etavirp    
+:xirtaMalacS tcejbo
+
+)"n\"(gnirtSkm.)"]" + _ + "["(pam.))" ,"(gnirtSkm._(pam.M        
+ = gnirtS :)(gnirtSot fed edirrevo    
+
+)ser(xirtaMalacS        
+)j()k(M.rehto * )k()i(M =+ )j()i(ser                    
+od N litnu 0 -< k rof                
+od N litnu 0 -< j rof            
+od N litnu 0 -< i rof        
+))N(]taolF[miDfo.yarrA()N(llif.yarrA = ser lav        
+/*        
+)yarrAot.)N(depuorg.)}        
+mus.)b * a >= )b ,a((pam.)))x(_(pam.M.rehto(piz.)y(M            
+)N / i ,N % i( = )tnI :y ,tnI :x( lav            
+{ >= )i ,v((pam.xednIhtiWpiz.)f0()N * N(llif.yarrA(xirtaMalacS        
+tcapmoc erom hcum ton dna wols ylsseldeen ylbaborp tub ynnuf */        
+ = xirtaMalacS :)xirtaMalacS :rehto(* fed    
+
+ezis.M = N lav etavirp    
+
+))ezis.)0(M == ezis._(llarof.M && ezis.)0(M == ezis.M(tressa    
+
+:)]]taolF[yarrA[yarrA :M lav( etavirp xirtaMalacS ssalc
+
+*.taN ,*.efasnu.evitanalacs tropmi
